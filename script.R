8d3c50fc12d247dd983c60dc72bd8564b160bbfc
Julian Regatky <julianregatky@gmail.com>
2020-06-23 18:34:46 -0300

    Update script.R

--- script.R
+++ script.R
@@ -32,6 +32,44 @@ for(i in 1:(ncol(dataset)-1)) {
 }
 dataset <- dataset[,setdiff(1:ncol(dataset),del_col)] # Eliminamos una de las duplicadas
 
+# La variable 'nac' es la única que cuenta con datos faltantes (56 obs con NA)
+# Esto representa sólo ~0.17% de las obs. Imputamos datos faltantes con el valor 2
+# que es el reportado para la variable en ~97.8% de los casos.
+sum(is.na(dataset$nac))/length(dataset$nac) # ~0.17%
+sum(dataset$nac[!is.na(dataset$nac)] == 2)/sum(!is.na(dataset$nac)) # ~97.8%
 
+dataset$nac[is.na(dataset$nac)] <- 2
 
+# ~~~~~~~~~~~~~~ MODELOS ~~~~~~~~~~~~~
 
+set.seed(123)
+index_train <- sample(1:nrow(dataset),round(nrow(dataset)*0.9))
+
+x_train <- dataset[index_train,] %>% select(-TARGET)
+x_validation <- dataset[setdiff(1:nrow(dataset),index_train),] %>% select(-TARGET)
+
+y_train <- dataset[index_train,'TARGET']
+y_validation <- dataset[setdiff(1:nrow(dataset),index_train),'TARGET']
+
+###########################
+###       LASSO         ###
+###########################
+
+# Cross-Validation, 10 folds
+x_train_matrix <- model.matrix( ~ .-1, x_train)
+cv.out = cv.glmnet(x_train_matrix, y_train, alpha = 1, nfolds = 10)
+plot(cv.out)
+lambda_star = cv.out$lambda.min
+
+# Estimamos el modelo con el lambda de CV
+model.lasso = glmnet(x = x_train_matrix,
+                     y = y_train,
+                     family = 'binomial',
+                     alpha = 1, # LASSO
+                     lambda = lambda_star)
+coef(model.lasso, s = lambda_star)
+
+x_validation_matrix <- model.matrix( ~ .-1, x_validation)
+pred.lasso = predict(model.lasso, s = lambda_star , newx = x_validation_matrix)
+ecm.lasso = sum( (pred.lasso - y_validation )^2   ) / 50
+log(ecm.lasso)
